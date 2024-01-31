8b0536e2bb64f088a9cd758edb65d162f428a828
Roldan Bacilio Puon <roldan.bacilio@hotmail.com>
2024-01-30 20:40:52 -0600

    Actualizar Repo

--- backend/controllers/tareasControllers.js
+++ backend/controllers/tareasControllers.js
@@ -1,18 +1,28 @@
-const getTareas = (req, res) => {
+const asyncHandler = require('express-async-handler')
+
+const getTareas = async (req, res) => {
     res.status(200).json({ mensajes: 'Get Tareas' })
 }
 
-const createTareas = (req, res) => {
+const createTareas = asyncHandler( async (req, res) => {
+    if(!req.body.desripcion){
+        res.status(400)
+        throw new Error('Por favor teclea una descripción')
+    }
+    
     res.status(201).json( {mensajes: 'Create Tareas' })
 }
+)
 
-const updateTareas = (req, res) => {
+const updateTareas = asyncHandler( async (req, res) => {
     res.status(200).json({ mensajes: `Modificar la tarea con id ${req.params.id}` })
 }
+)
 
-const deleteTareas = (req, res) => {
+const deleteTareas = asyncHandler( async (req, res) => {
     res.status(200).json({ id: req.params.id })
 }
+)
 
 module.exports ={
     getTareas,
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const connectDB = require('./config/db')
+const colors =  require('colors')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require ('./middleware/errorMiddleware')
 const port = process.env.PORT || 8000
 const app = express()
+connectDB()
+app.use(express.json()) // Middleware para parsear el body de las peticiones
+app.use(express.urlencoded({extended: false}))
 app.use('/api/tareas', require('./routes/tareasRoutes'))
+app.use(errorHandler)
 app.listen(port, () => console.log(`Servidor inicializado en el puerto ${port}!`))
 
 //app.get('/api/tareas', (req, res) =>{
