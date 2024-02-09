d079092e259014f0e1d3f31eda3bfe80d86a3b47
Roldan Bacilio Puon <roldan.bacilio@hotmail.com>
2024-02-08 19:24:10 -0600

    Agregando cors

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,10 @@ const colors =  require('colors')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require ('./middleware/errorMiddleware')
 const port = process.env.PORT || 8000
+const cors = require('cors')
 const app = express()
 connectDB()
+app.use(cors())
 app.use(express.json()) // Middleware para parsear el body de las peticiones
 app.use(express.urlencoded({extended: false}))
 app.use('/api/tareas', require('./routes/tareasRoutes'))
